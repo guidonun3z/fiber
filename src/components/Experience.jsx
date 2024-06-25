4949707e3053cac2cba93af01a85c0d5128ac12f
Guido Nuñez <dreamguido@gmail.com>
2024-06-25 15:20:02 -0500

    primera subida

---
+++ src/components/Experience.jsx
@@ -0,0 +1,20 @@
+import {OrbitControls} from "@react-three/drei"
+import { Office } from "./Office";
+
+export const Experience = () => {
+    return (
+        <>
+            <ambientLight intensity={1} />
+            <OrbitControls />
+            <Office />
+            {/* {
+                // <mesh>
+                //     <boxGeometry />
+                //     <meshNormalMaterial />
+                // </mesh> 
+
+            } */}
+
+        </>
+    );
+};
